f9b4c26eb80cfacd21ae61b2d8951fce445b1541
Vitor <vitorcostaofficial@gmail.com>
2025-09-01 22:47:26 -0300

    feat: implementação da busca de funcionários por id

--- src/main/java/com/analista/cadastro_email_api/api/controller/FuncionarioController.java
+++ src/main/java/com/analista/cadastro_email_api/api/controller/FuncionarioController.java
@@ -1,24 +1,41 @@
 package com.analista.cadastro_email_api.api.controller;
 import com.analista.cadastro_email_api.domain.model.Funcionario;
+import com.analista.cadastro_email_api.domain.model.FuncionarioId;
 import com.analista.cadastro_email_api.domain.repository.FuncionarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+import java.util.Optional;
+@RequestMapping("/funcionarios")
 @AllArgsConstructor
 @RestController
 public class FuncionarioController {
 
     private final FuncionarioRepository funcionarioRepository;
 
-    @GetMapping("/funcionarios")
+    @GetMapping
     public List<Funcionario> listar() {
         return funcionarioRepository.findAll();
     }
 
+    @GetMapping("/{empresa}/{filial}/{matricula}")
+    public ResponseEntity<Funcionario> buscar(@PathVariable String empresa, @PathVariable String filial, @PathVariable String matricula) {
+
+        FuncionarioId funcionarioId = new FuncionarioId(empresa, filial, matricula);
+        Optional<Funcionario> funcionario = funcionarioRepository.findById(funcionarioId);
+
+        if (funcionario.isPresent()) {
+            return ResponseEntity.ok(funcionario.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
